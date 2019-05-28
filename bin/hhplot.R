faff8fe7e3414a24adff0dfd536096d5f04fce8e
Thomas Hackl <thackl@lim4.de>
2019-05-28 14:27:19 -0400

    add pfam acc to table

--- bin/hhplot.R
+++ bin/hhplot.R
@@ -39,7 +39,7 @@ if(length(args) == 4){
   hits_0 <- hits_0 %>%
     mutate(pfam_acc = str_replace(description, " .*", ""), description = NULL) %>%
     left_join(pfam_1) %>%
-    select(i, description, everything())
+    select(i, description, pfam_acc, everything())
 }
 
 xmax <- as.numeric(head_0["Match_columns"])
@@ -58,23 +58,31 @@ hits_2 <- hits_1 %>% group_by(bin) %>% top_n(10, -evalue) %>%
   ungroup %>% select(-bin) %>% unique %>% arrange(evalue) %>%
   mutate(y=-row_number())
 
+hits_2 %<>% mutate(
+  target_full_start = query_start - target_start,
+  target_full_end = query_end + (target_cols - target_end)
+)
+
+
 gg_hits <- ggplot(hits_2) +
 #  geom_segment(aes(x=(query_start+query_end)/2, xend=xmax*1.08, y=y, yend=(y+1)*2-1), hits_0[top_rows,], alpha=.2) +
-  geom_vline(xintercept = c(0,xmax)) +
+  geom_vline(xintercept = c(0,xmax), linetype=3) +
+  geom_vline(xintercept = c(-0.05*xmax,1.05*xmax)) +
+  geom_segment(aes(x=target_full_start, xend=target_full_end, y=y, yend=y),
+               color="black", size=1) +
   geom_segment(aes(x=query_start, xend=query_end, y=y, yend=y,
                    color=evalue_color(evalue, min_evalue, max_evalue)), size=3) + #-log10(evalue))) +
-  geom_point(aes(x=query_end*1.04, y=y+.1), color="grey90", size=5) + 
-  geom_text(aes(x=query_end*1.042, y=y, label=i), size=4, hjust=0.5) + 
+  geom_point(aes(x=query_end*1.04, y=y+.1), color="grey90", size=5) +
+  geom_text(aes(x=query_end*1.042, y=y, label=i), size=4, hjust=0.5) +
   #  geom_text(aes(y=(y+1)*2-1, label=description), x=xmax*1.1, hits_0[top_rows,], size=3, hjust=0) +
-  xlim(0, xmax*1.05) +
-  ylim(-50,0) +
-  expand_limits(color=-log10(c(max_evalue, min_evalue))) + 
+  expand_limits(color=-log10(c(max_evalue, min_evalue))) +
   scale_color_distiller(palette="Spectral") +
   ggtitle(str_replace(args[3], ".pdf", "")) +
+  coord_cartesian(xlim=c(0, xmax), ylim=c(-50,0)) +
   theme_classic() +
   theme(legend.position = "bottom") + no_y_axis()
 
-gg_table <- ggtexttable(select(hits_2, 1:4), rows = NULL)
+gg_table <- ggtexttable(hits_2[1:30,1:5], rows = NULL)
 
 gg <- gg_hits + gg_table + plot_layout(widths=c(1,1))
 ggsave(args[3], gg, width=16, height=11)
